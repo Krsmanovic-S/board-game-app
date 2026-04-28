bb1375d5a4e38ed637b4da9faa06bcae26c404a5
Stefan Krsmanovic <103185975+Krsmanovic-S@users.noreply.github.com>
2026-04-28 22:59:47 +0200

    Firebase Config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,16 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.krsmanovic.kockica"
+    namespace = "com.krsmanovic.boardgametracker"
+    applicationId = "com.krsmanovic.boardgametracker"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
